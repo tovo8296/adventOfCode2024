c23b6cb40f6f0c920c061004f35d4c8b9cddcabd
Tobi <mail@glueckstobi.de>
2025-12-06 13:11:46 +0100

    day 11, part 2

--- src/main/kotlin/day11/Day11.kt
+++ src/main/kotlin/day11/Day11.kt
@@ -1,32 +1,39 @@
 package day11
 
 fun main() {
-    val stones = input.split(" ").map { it.toLong() }.toMutableList()
-//    println("init: ${stones.joinToString()}")
-    (1 .. 25).forEach {
-        blink(stones)
-//        println("after $it blinks: ${stones.joinToString()}")
+    var stones = mapOf(*input.split(" ").map { it.toLong() }.map { it to 1L }.toTypedArray())
+
+    (1 .. 75).forEach {
+        stones = blink(stones)
+        println("after $it blinks: ${stones.size} numbers: $stones")
     }
-    println("Stone count: ${stones.size}")
+
+    val sum = stones.values.sum()
+    println("Stone count: $sum")
 }
 
-fun blink(stones: MutableList<Long>) {
-    val iter = stones.listIterator()
-    while(iter.hasNext()) {
-        val next = iter.next()
-        if (next == 0L) {
-            iter.set(1L)
-        }else if (next.toString().length % 2 == 0) {
-            val s = next.toString()
+fun blink(stones: Map<Long, Long>): Map<Long, Long> {
+    val result = mutableMapOf<Long, Long>()
+    stones.forEach { number, times ->
+        if (number == 0L) {
+            addToMap(result, 1L, times)
+        }else if (number.toString().length % 2 == 0) {
+            val s = number.toString()
             val half = s.length / 2
             val i1 = s.substring(0, half).toLong()
             val i2 = s.substring(half).toLong()
-            iter.set(i1)
-            iter.add(i2)
+            addToMap(result, i1, times)
+            addToMap(result, i2, times)
         }else {
-            iter.set(next * 2024L)
+            addToMap(result, number * 2024L, times)
         }
     }
+    return result
+}
+
+fun addToMap(map: MutableMap<Long, Long>, number: Long,times: Long) {
+    val previous = map.getOrElse(number, { 0 })
+    map.set(number, previous + times)
 }
 
 val input = "4 4841539 66 5279 49207 134 609568 0"
